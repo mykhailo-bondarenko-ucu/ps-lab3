515d59e524b2b10561124cd58e3dced576339a4c
michael-2956 <work.michael.2956@gmail.com>
2023-05-23 15:35:32 +0300

    remove code from main

--- facade-service/message.py
+++
@@ -1,9 +0,0 @@
-import uuid
-
-class Message:
-    def __init__(self, msg) -> None:
-        self.msg = msg
-        self.uuid = str(uuid.uuid4())
-
-    def json(self):
-        return {"text": self.msg, "uuid": self.uuid}
--- logging-service/message.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-
-class MessageBody(BaseModel):
-    text: str
-    uuid: str
